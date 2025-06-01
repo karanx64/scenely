cde83dc6a2c604e644cbe30c2081b0bd847b2dd0
Karanx64 <karansinghx64@gmail.com>
2025-06-02 03:51:41 +0530

    scenely

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ import {
 } from "lucide-react";
 import { NavLink } from "react-router-dom";
 import ThemeSwitcher from "./ThemeSwitcher";
-import scenely from "../../src/assets/Scenely.png";
+import scenely from "../../public/scenely.png";
 import { useState } from "react";
 
 const navItems = [
